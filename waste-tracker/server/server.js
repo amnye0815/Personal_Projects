d84c420883f616db595ee0b285b2d1b5735aed2a
Austin Nye <amnye0815@gmail.com>
2022-11-17 10:32:28 -0800

    set up continued

--- waste-tracker/server/server.js
+++ waste-tracker/server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 8000;
 
 app.listen(port, () => console.log('Listening on port: ${port}'));
+app.use(cors());
