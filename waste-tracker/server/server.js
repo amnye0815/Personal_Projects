5586521675981466552ed55e4f7b51da1d93072e
Austin Nye <amnye0815@gmail.com>
2022-11-22 15:13:37 -0800

    basic set up complete

--- waste-tracker/server/server.js
+++ waste-tracker/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('./config/mongoose.config');
 require('./routes/item.routes')(app);
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.listen(8000, () => console.log('Listening on port 8000'));
 app.use(cors());
