0fce7faefe6894b5693f2cc855f2644ffa6dd5e9
Austin Nye <amnye0815@gmail.com>
2022-11-22 14:31:50 -0800

    adding to controller

--- waste-tracker/server/controllers/item.controller.js
+++ waste-tracker/server/controllers/item.controller.js
@@ -1,5 +1,17 @@
+const Item = require('../models/item.model');
 const { response } = require("express");
 
 module.exports.index = (req, res) => {
     response.json({ message: "Hello World" });
+}
+
+module.exports.createItem = (req, res) => {
+    const { name, weight, cost } = req.body;
+    Item.create({
+        name,
+        weight, 
+        cost
+    })
+    .then(item=> res.json(item))
+    .catch(err=> res.json(err));
 }
